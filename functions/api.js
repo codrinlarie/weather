5894d5301e4fdaaa79feb365cb1f652c67d70897
codrinlarie <codrinlarie@hotmail.com>
2024-08-09 11:09:49 +0100

    update

--- functions/api.js
+++ functions/api.js
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
     res.json({ message: 'Hello from Express!' });
   });
 
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 const apiKey = process.env.API_KEY;
 
 app.get('/weather', async (req, res) => {
@@ -49,8 +49,8 @@ app.get('/weather', async (req, res) => {
     }
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+// app.listen(PORT, () => {
+//     console.log(`Server is running on port ${PORT}`);
+// });
 
 module.exports.handler = serverless(app);
