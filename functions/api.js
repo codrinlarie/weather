41bda7d9704b08f6438d017ff3596ae3e244e1aa
codrinlarie <codrinlarie@hotmail.com>
2024-08-09 10:27:20 +0100

    update

--- functions/api.js
+++ functions/api.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const app = express();
 
 app.use(cors({
-    origin: 'https://codrinlarie.github.io/cloudyskies/'
+    origin: 'https://codrinlarie.github.io/'
   }));
 
 app.get('/', (req, res) => {
